5286995979681de870f45d583d608d974ead9f10
Marcais II <33102220+WebbyD@users.noreply.github.com>
2018-06-21 23:56:47 -0400

    Add files via upload

---
+++ me/am.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    alert("I'm from Baltimore, Maryland, later moved to Harford County when I was 2. Graduated from Kennedy Krieger High School on June 7th, 2013 then studied Computer Information Systems at Harford Community College. Started on September 2nd 2014, graduated May 24th 2018. GPA of 3.30 and I love web development!!");
+}
+
+function myFunction2(){
+	alert("Also....HOORAY FOR WEB DEVELOPMENT!!!!!") 
+}
